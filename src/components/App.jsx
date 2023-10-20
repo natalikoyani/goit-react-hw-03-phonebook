73b4371864889a4171a6c37da29913c1c3372977
natalikoyani <natalikagami@gmail.com>
2023-10-20 15:06:02 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
-import { Component } from "react";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
     contacts: [],
     filter: '',
-  }
+  };
 
   componentDidMount() {
     try {
-      const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
-      this.setState({contacts: parsedContacts})
+      const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+      this.setState({ contacts: parsedContacts });
     } catch (error) {
       console.log(error.message);
     }
@@ -21,34 +21,42 @@ export class App extends Component {
 
   componentDidUpdate(prevState) {
     if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
 
   addContact = newContact => {
-    if (this.state.contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
-      return alert(`${newContact.name} is already in contacts!`)
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      return alert(`${newContact.name} is already in contacts!`);
     }
-    
+
     this.setState(prevState => ({
-      contacts: [ ...prevState.contacts, { ...newContact, id: nanoid() }]
-    }))
-  }
+      contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+    }));
+  };
 
   onFilterChange = evt => {
-    this.setState({filter: evt.target.value})
-  }
+    this.setState({ filter: evt.target.value });
+  };
 
-  deleteContact = (id) => {
+  deleteContact = id => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id)
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
-  }
+  };
 
   render() {
     const { contacts, filter } = this.state;
 
-    const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+    const filteredContacts = contacts.filter(contact => {
+      return filter
+        ? contact.name.toLowerCase().includes(filter.toLowerCase())
+        : true;
+    });
 
     return (
       <div>
@@ -58,6 +66,6 @@ export class App extends Component {
         <Filter items={filteredContacts} onChange={this.onFilterChange} />
         <ContactList items={filteredContacts} onDelete={this.deleteContact} />
       </div>
-    )
+    );
   }
-};
+}
